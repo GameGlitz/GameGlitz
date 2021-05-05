f9f640d6b8257af990d4aab691d41d85af9ca4c5
GameGlitz <contact@gameglitz.dev>
2021-05-05 21:46:52 +0100

    Cleaned.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,27 +8,23 @@
 /**
  * Applies the date manipulation to the document.
  */
-const applyDate = async () => {
-    date = new Date();
-    document.getElementById("terminal-date").innerHTML = date;
+const applyDate = async() => {
+    dateElement = "terminal-date";
+    dateString = new Date().toLocaleString();
+
+    document.getElementById(dateElement).innerHTML = dateString;
 }
 
 /**
  * Applies the protocol identifier maniplation to the document.
  */
-const applyProtocol = async () => {
-    const fetchData = async (url) => {
-        const res = await fetch(url);
-        return await res.text();
-    }
+const applyProtocol = async() => {
+    protocolElement = "terminal-protocol"
+    protocolRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/;
+    protocolFetch = await fetch('https://gameglitz.dev/cdn-cgi/trace');
 
-    fetchData('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
-        let protocolRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-        let protocolIdentifier = data.match(protocolRegex)[0];
-        document.getElementById("terminal-protocol").innerHTML = protocolIdentifier;
-    });
+    await (protocolFetch).text().then(data => document.getElementById(protocolElement).innerHTML = data.match(protocolRegex)[0]);
 }
-
 
 applyDate();
 applyProtocol();
